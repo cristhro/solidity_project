de8e8c049137a079082e81b31ca0afcd40d53024
Cristhian <cristhianrodriguez.dev@gmail.com>
2024-03-27 22:58:00 +0100

    FIX: deployed

--- Hardhat/scripts/deploy.ts
+++ Hardhat/scripts/deploy.ts
@@ -1,19 +1,21 @@
 import { ethers } from "hardhat";
 
 
-async function _deployContract(contractName: string) {
-  const Contract = await ethers.getContractFactory("LegalETH");
-  const contract = await Contract.deploy();
+async function _deployContract(contractName: string, ...args: Array<any>) {
+  const Contract = await ethers.getContractFactory(contractName);
+  const contract = await Contract.deploy(...args);
   await contract.deployed();
+  console.log(`Contract ${contractName} (${args}) deployed to:`, contract.address);
+
   return contract;
 }
 
 async function main() {
 // We get the contract to deploy
   const legalETH = await _deployContract("LegalETH")
-
-  console.log("Contract disney deployed to:", legalETH.address);
-
+  const legalCase = await _deployContract("LegalCase")
+  const legalPackages = await _deployContract("LegalPackages", legalETH.address)
+  const legalManager = await _deployContract("LegalManager", legalETH.address, legalCase.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
