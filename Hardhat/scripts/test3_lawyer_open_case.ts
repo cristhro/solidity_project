915163d2c27522469784deb47f26a9b72b192aea
Jesus <jesusrosas.dm@gmail.com>
2024-03-27 23:13:12 -0600

    Add test3 open case

---
+++ Hardhat/scripts/test3_lawyer_open_case.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+const {  LEGAL_CASE_ADDRESS, LEGAL_MANAGER_CONTRACT_ADDRESS, LEGAL_ETH_CONTRACT_ADDRESS, CLIENT_ADDRESS } = process.env;
+
+async function main() {
+  
+    const LegalManagerContract = await ethers.getContractAt('LegalManager', LEGAL_MANAGER_CONTRACT_ADDRESS);
+    const tx = await LegalManagerContract.openLegalCase(CLIENT_ADDRESS, 'case_1000');
+    await tx.wait();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
